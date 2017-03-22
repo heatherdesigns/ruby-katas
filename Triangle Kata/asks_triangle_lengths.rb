67cddb4c20edffff8fcc34459882a176642a5faa
Heather Taylor <heather@heysparkbox.com>
2017-03-22 14:53:11 -0400

    feat: add right angle triangle output
    Add right angle triangle output option.

--- Triangle Kata/asks_triangle_lengths.rb
+++ Triangle Kata/asks_triangle_lengths.rb
@@ -6,11 +6,6 @@ require "./right_angle"
 
 class AskTriangleLengths
   
-  x = 0;
-  y = 0;
-  z = 0;
-  t = [];
-  
   def enter_lengths
     puts "Enter the first of three lengths: "
     x = gets.chomp.to_i
@@ -30,62 +25,31 @@ class AskTriangleLengths
       puts "Enter the third length: "
       z = gets.chomp.to_i
       t = Triangle.new(x, y, z)
-    end # end while validation looop
+    end 
     
-  end # end enter_lengths
- 
- 
-    
-    # Example: 2, 2, 3
     triangles = [
-      Scalene.new(x, y, z), # true, Not to return this
-      Isosceles.new(x, y, z), # true, Return this 
-      Equilateral.new(x, y, z), # not true
-      RightAngle.new(x, y, z)
-    ]
+      Scalene.new(x, y, z).validate_type, 
+      Isosceles.new(x, y, z).validate_type,
+      Equilateral.new(x, y, z).validate_type 
+    ]  
     
+    if triangles[2] == true
+      puts "The sides #{x}, #{y} and #{z} make an equilateral triangle."
+    elsif triangles[1] == true
+      puts "The sides #{x}, #{y} and #{z} make an isosceles triangle."
+    else
+      puts "The sides #{x}, #{y} and #{z} make a scalene triangle." 
+    end  
     
-     # Return the triangle instance that the sides represent
-     # In the example above, the second triangle (Isoceles) should be returned
+    if RightAngle.new(x, y, z).validate_type == true
+      puts "They also make a right angle triangle."
+    end
+  
+  end 
     
-end # end class
-
+end 
 
 triangle = AskTriangleLengths.new.enter_lengths
-
-
-
-
-=begin NO LONGER A THING
-# find type of triangle
-
-triangles.each do |t|
-  puts t.validate_type
-end
-=end
-
-=begin
-triangle = Equilateral.new(triangle.sides[0], triangle.sides[1], triangle.sides[2])
-if triangle.validate_type_equilateral?
-  puts "This is an equilateral triangle."
-end
-
-triangle = Isosceles.new(triangle.sides[0], triangle.sides[1], triangle.sides[2])
-if triangle.validate_type_isosceles?
-  puts "This is an isosceles triangle."
-end
-
-triangle = Scalene.new(triangle.sides[0], triangle.sides[1], triangle.sides[2])
-if triangle.validate_type_scalene?
-  puts "This is a scalene triangle."
-end
-
-triangle = RightAngle.new(triangle.sides[0], triangle.sides[1], triangle.sides[2])
-if triangle.validate_right_angle?
-  puts "This is a right angle triangle."
-end
-=end
-
 
 
 
